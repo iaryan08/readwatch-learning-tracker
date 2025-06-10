75bb8f0aa4f655c0fa34adb7f6f6c5225310ded0
Aryan <jhariyaaryan118@gmail.com>
2025-06-10 17:31:02 +0530

    Add .next/static/chunks/src_app_profile_[username]_page_tsx_a993527f._.js 1749556856477

---
+++ .next/static/chunks/src_app_profile_[username]_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_56a45c24._.js",
+  "static/chunks/src_f7fb7e43._.js"
+],
+    source: "dynamic"
+});
